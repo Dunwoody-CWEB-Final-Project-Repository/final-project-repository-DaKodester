1fcaa5a29e361f2c0f994d3e5619ff882864e526
cody <dricodp@dunwoody.edu>
2021-03-19 17:36:28 -0500

    adding week 8/9

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?PHP
-	
+	include 'config/database.php';
+
+    // Create insert statement
+    $query="INSERT into appointments (appointmentID, userID, adminID ) VALUES (1234, 12345, 123456)";
+    // run insert
+    $stmt=$con->prepare($query);
+    $stmt->execute();
 ?>
 
 
@@ -11,28 +17,29 @@
 -->
 <html>
 	<head>
-		<title>Contact - Twenty by HTML5 UP</title>
+		<title>Cody's Car Wash</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
-	<body class="contact is-preload">
+	<body class="index is-preload">
 		<div id="page-wrapper">
 
 			<!-- Header -->
-				<header id="header">
-					<h1 id="logo"><a href="index.html">Twenty <span>by HTML5 UP</span></a></h1>
+				<header id="header" class="alt">
+					<h1 id="logo"><a href="index.html">Cody's <span>Car Wash</span></a></h1>
 					<nav id="nav">
 						<ul>
 							<li class="current"><a href="index.html">Welcome</a></li>
 							<li class="submenu">
-								<a href="#">Layouts</a>
+								<a href="#">Menu</a>
 								<ul>
-									<li><a href="left-sidebar.html">Left Sidebar</a></li>
-									<li><a href="right-sidebar.html">Right Sidebar</a></li>
+									<li><a href="left-sidebar.html">About Me</a></li>
+									<li><a href="right-sidebar.html">Recent Projects</a></li>
 									<li><a href="no-sidebar.html">No Sidebar</a></li>
 									<li><a href="contact.html">Contact</a></li>
+									<li><a href="contact.html">Log-In</a></li>
 									<li class="submenu">
 										<a href="#">Submenu</a>
 										<ul>
@@ -46,50 +53,73 @@
 								</ul>
 							</li>
 							<li><a href="#" class="button primary">Sign Up</a></li>
+							<li><a href="contact.html" class="button primary">Log-In</a></li>
 						</ul>
 					</nav>
 				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+
+					<!--
+						".inner" is set up as an inline-block so it automatically expands
+						in both directions to fit whatever's inside it. This means it won't
+						automatically wrap lines, so be sure to use line breaks where
+						appropriate (<br />).
+					-->
+					<div class="inner">
+
+						<header>
+							<h2>Cody's Car Wash</h2>
+						</header>
+						<p>This is Cody's Car Wash, Click Down
+						<br />
+						Below To Schedule an Appointment
+						<br />
+						<footer>
+							<ul class="buttons stacked">
+								<li><a href="#main" class="button fit scrolly">Schedule An Appointment</a></li>
+							</ul>
+						</footer>
+
+					</div>
+
+				</section>
 
 			<!-- Main -->
 				<article id="main">
 
 					<header class="special container">
-						<span class="icon solid fa-envelope"></span>
-						<h2>Get In Touch</h2>
-						<p>Use the form below to give /dev/null a piece of your mind.</p>
+						<span class="icon solid fa-chart-bar"></span>
+						<h2>As this is my <strong>twentieth</strong> freebie for HTML5 UP
+						<br />
+						I decided to give it a really creative name.</h2>
+						<p>Turns out <strong>Twenty</strong> was the best I could come up with. Anyway, lame name aside,
+						<br />
+						it's minimally designed, fully responsive, built on HTML5/CSS3,
+						and, like all my stuff,
+						<br />
+						released for free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0</a> license. Have fun!</p>
 					</header>
-
-					<!-- One -->
-						<section class="wrapper style4 special container medium">
-
-							<!-- Content -->
-								<div class="content">
-									<form>
-										<div class="row gtr-50">
-											<div class="col-6 col-12-mobile">
-												<input type="text" name="name" placeholder="Name" />
-											</div>
-											<div class="col-6 col-12-mobile">
-												<input type="text" name="email" placeholder="Email" />
-											</div>
-											<div class="col-12">
-												<input type="text" name="subject" placeholder="Subject" />
-											</div>
-											<div class="col-12">
-												<textarea name="message" placeholder="Message" rows="7"></textarea>
-											</div>
-											<div class="col-12">
-												<ul class="buttons">
-													<li><input type="submit" class="special" value="Send Message" /></li>
-												</ul>
-											</div>
-										</div>
-									</form>
-								</div>
-
-						</section>
-
 				</article>
+
+					
+
+			<!-- CTA -->
+				<section id="cta">
+
+					<header>
+						<h2>Ready to do <strong>something</strong>?</h2>
+						<p>Proin a ullamcorper elit, et sagittis turpis integer ut fermentum.</p>
+					</header>
+					<footer>
+						<ul class="buttons">
+							<li><a href="#" class="button primary">Take My Money</a></li>
+							<li><a href="#" class="button">LOL Wut</a></li>
+						</ul>
+					</footer>
+
+				</section>
 
 			<!-- Footer -->
 				<footer id="footer">
@@ -114,7 +144,6 @@
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/jquery.dropotron.min.js"></script>
 			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.scrollgress.min.js"></script>
 			<script src="assets/js/jquery.scrollex.min.js"></script>
 			<script src="assets/js/browser.min.js"></script>
 			<script src="assets/js/breakpoints.min.js"></script>
